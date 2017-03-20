f721eb4f392ef4b142c5200960d28ea50272f49e
Willy Nugraha <willymiiee@gmail.com>
2017-03-20 14:52:15 +0700

    add auth & username column on users (unique)

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('username');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->unique(array('username', 'email'));
         });
     }
 
