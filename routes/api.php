7463ce2cd430233a830a2adf3df045b5e150d421
Willy Nugraha <willymiiee@gmail.com>
2017-05-15 22:12:46 +0700

    change routes for MUA

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('/mua', 'MUAController');
+Route::resource('muas', 'MUAController');
